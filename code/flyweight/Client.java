a203fd00f2393bd95a1d41174e4064c7489fe6bb
RainyTong <littlelittletong@gmail.com>
2019-01-02 10:10:38 +0800

    Add flyweight pattern.

---
+++ code/flyweight/Client.java
@@ -0,0 +1,25 @@
+public class Client {
+    private static final String colors[] = {"Red", "Green", "Blue", "White", "Black"};
+
+    private static String getRandomColor() {
+        return colors[(int)(Math.random() * colors.length)];
+    }
+
+    private static int getRandomX() {
+        return (int) (Math.random()*100);
+    }
+
+    private static int getRandomY() {
+        return (int) (Math.random()*100);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0;i<20;i++) {
+            Circle circle =(Circle) ShapeFactory.getCircle(getRandomColor());
+            circle.setX(getRandomX());
+            circle.setY(getRandomY());
+            circle.setRadius(100);
+            circle.draw();
+        }
+    }
+}
